7704714210451dab93b9e0bca1cdfcfcc060c6b3
Hamza Memon <hmemon67@gmail.com>
2018-03-12 12:30:27 -0400

    Got video and frames to work

--- Sprint4/js/main.js
+++ Sprint4/js/main.js
@@ -27,7 +27,27 @@ function startVideo(){
     drawImage(video, 0, 0, canvas.width, canvas.height);
     $scope.sendPicture();
 };
-
+//Graph variables
+    var numMales = 0
+    var numFemales = 0
+    var ages = {
+        age02 : 0,
+        age37 : 0,
+        age812 : 0,
+        age1319 : 0,
+        age2036 : 0,
+        age3765 : 0,
+        age66 : 0
+    };
+    var ethnicities = {
+        numCauc : 0,
+        numHisp : 0,
+        numAfrc : 0,
+        numAsia : 0
+    };
+    
+    
+    
 var constraints = {
   audio: false,
   video: true
@@ -82,9 +102,10 @@ $scope.analyzeImage = function (base64Image, $q) {
     success: function (data) {
       promise.resolve(data);
       console.log('Success');
+      readJSON(data);
       console.log("data",data);
-        // call graph update function from d3 js
       console.log("promise",promise);
+      
     },
     error: function (data) {
       promise.reject(data);
@@ -101,6 +122,56 @@ $scope.analyzeImage = function (base64Image, $q) {
   return promise.promise;
 };
 
+function readJSON(data){
+    var numpeople = data.length;
+    numMales = 0;
+    numFemales = 0;
+    ages.age02 = 0;
+    ages.age37 = 0;
+    ages.age812 = 0;
+    ages.age1319 = 0;
+    ages.age2036 = 0;
+    ages.age3765 = 0;
+    ages.age66 = 0;
+    ethnicities.numCauc = 0;
+    ethnicities.numHisp = 0;
+    ethnicities.numAfrc = 0;
+    ethnicities.numAsia = 0;
+    for (var i = 0; i < numPeople; i++){
+        if (data.exposures[i].predictions[0].observations[0].focus.name =="Male"){
+            numMales++;
+        }else{
+            numFemales++;
+        }
+        ageGuess = data.exposures[i].predictions[1].observations[0].focus.name;
+        if(ageGuess == "0-2"){
+            ages.age02++;
+        }else if(ageGuess == "3-7"){
+            ages.age37++;
+        }else if(ageGuess == "8-12"){
+            ages.age812++;
+        }else if(ageGuess == "13-19"){
+            ages.age1319++;
+        }else if(ageGuess == "20-36"){
+            ages.age2036++;
+        }else if(ageGuess == "37-65"){
+            ages.age3765++;
+        }else if(ageGuess == "66+"){
+            ages.age66++;
+        }
+        ethnGuess = data.exposures[i].predictions[3].observatopms[1].focus.name;
+        if(ethnGuess == "Caucasian"){
+            ethnicities.numCauc++;
+        }else if(ethnGuess == "Hispanic"){
+            ethnicities.numHisp++;
+        }else if(ethnGuess == "African"){
+            ethnicities.numAfrc++;
+        }else if(ethnGuess == "Asian"){
+            ethnicities.numAsia++;
+        }
+    }
+};    
+    
 navigator.mediaDevices.getUserMedia(constraints).
   then(handleSuccess).catch(handleError);
 
